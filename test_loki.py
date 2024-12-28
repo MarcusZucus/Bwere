832005220f3a2d2ad729143fb04fdbab299fce20
MarcusZucus <marcozucoia@gmail.com>
2024-12-28 19:03:00 +0100

    Update1 test_loki.py

--- test_loki.py
+++ test_loki.py
@@ -1,4 +1,8 @@
 from modules.loki_conversation import start_loki_conversation
 
+def main():
+    print("Firebase initialized. Ahora puedes hablar directamente como Loki. Escribe 'salir' para terminar.\n")
+    start_loki_conversation(user_id="loki")  # Asumimos que siempre eres Loki
+
 if __name__ == "__main__":
-    start_loki_conversation()
+    main()
