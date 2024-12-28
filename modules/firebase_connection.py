ff39087eac0433a31dbab462c8b1848cbc23c439
MarcusZucus <marcozucoia@gmail.com>
2024-12-28 21:28:31 +0100

    Update firebase_connection.py

--- modules/firebase_connection.py
+++ modules/firebase_connection.py
@@ -8,21 +8,31 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 _firebase_app = None
+_firestore_client = None
 
 def init_firebase():
     """
     Inicializa la app de Firebase si no está ya inicializada.
     """
-    global _firebase_app
+    global _firebase_app, _firestore_client
     if not _firebase_app:
         cred_path = os.getenv("FIREBASE_CRED_PATH", "serviceAccountKey.json")
         cred = credentials.Certificate(cred_path)
         _firebase_app = firebase_admin.initialize_app(cred)
+        print("Firebase inicializado.")
+    else:
+        print("Firebase ya estaba inicializado.")
+
+    # Configurar Firestore client si no está configurado
+    if _firestore_client is None:
+        _firestore_client = firestore.client()
+        print("Firestore cliente configurado.")
 
 def get_firestore_client():
     """
     Retorna un cliente de Firestore, iniciando Firebase si fuera necesario.
     """
-    if not firebase_admin._DEFAULT_APP_NAME in firebase_admin._apps:
+    global _firestore_client
+    if _firestore_client is None:
         init_firebase()
-    return firestore.client()
+    return _firestore_client
