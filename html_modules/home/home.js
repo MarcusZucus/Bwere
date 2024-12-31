ec8c7ca89ee289aaa50d38fb7657249ebedf7b1a
MarcusZucus <marcozucoia@gmail.com>
2024-12-31 12:15:55 +0100

    Update home.js

--- html_modules/home/home.js
+++ html_modules/home/home.js
@@ -1,12 +1,3 @@
-const brandDropdown = document.getElementById('brand-dropdown');
-const connectButton = document.getElementById('connect-button');
-
-// Agrega funcionalidad al botón
-connectButton.addEventListener('click', () => {
-  const selectedBrand = brandDropdown.value;
-  if (selectedBrand) {
-    alert(`Has seleccionado ${selectedBrand}. ¡Conectando tu smartwatch!`);
-  } else {
-    alert('Por favor, selecciona tu smartwatch antes de continuar.');
-  }
+document.querySelector('.SeleccionaUnaMarca').addEventListener('click', () => {
+  alert('¡Selecciona una marca de smartwatch para continuar!');
 });
