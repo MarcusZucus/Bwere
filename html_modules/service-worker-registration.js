a6e6b2f46f1390b047ffcdf7ca99053fbc03eecc
MarcusZucus <marcozucoia@gmail.com>
2025-01-08 23:43:47 +0100

    Create service-worker-registration.js

---
+++ html_modules/service-worker-registration.js
@@ -0,0 +1,12 @@
+// service-worker-registration.js
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/html_modules/service-worker.js')
+            .then(function(registration) {
+                console.log('Service Worker registered with scope:', registration.scope);
+            })
+            .catch(function(error) {
+                console.log('Service Worker registration failed:', error);
+            });
+    });
+}
