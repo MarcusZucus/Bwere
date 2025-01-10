46ef05e6595735941e5fe2b7206e4e4fe5e8ffe0
MarcusZucus <marcozucoia@gmail.com>
2025-01-10 13:48:36 +0100

    Create logging_utils.py

---
+++ AutomaticApis/logging_utils.py
@@ -0,0 +1,12 @@
+import logging
+
+def setup_logger(log_file):
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        handlers=[
+            logging.FileHandler(log_file),
+            logging.StreamHandler()
+        ]
+    )
+    return logging.getLogger()
