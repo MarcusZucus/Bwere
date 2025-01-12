90f0c4b48f3058399cd452a34364dcf78e45009f
Marco <marcozucoia@gmail.com>
2025-01-13 00:45:52 +0100

    Actualización del Service Worker generado con Workbox# Please enter the commit message for your changes. Lines starting

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@ setCacheNameDetails({
 });
 
 // Precacheo automático con Workbox
-precacheAndRoute(self.__WB_MANIFEST || []);
+precacheAndRoute([{"revision":"6da9b85404e5654454c49b6e802b3802","url":"home/exchangeToken.js"},{"revision":"3777c9ecad59e4ca0d7d5e8f745d7ca6","url":"home/home.html"},{"revision":"8aeb73e69a3fbc84bf948f241cc8cac8","url":"home/responsive.css"},{"revision":"e7e0797046e52b2eb763ee8f493baf5c","url":"modules/animations.js"},{"revision":"0d4e80fed94561a3728a55616d8ad3fe","url":"modules/chat.js"},{"revision":"23e23e9fca7a07856022ac061ad571af","url":"modules/gpt.js"},{"revision":"06cbd0ce2a8bc8c28e27ee645fcd073a","url":"modules/menu.js"},{"revision":"e64f64b2be8e88db509525aaf82221c8","url":"styles.css"}] || []);
 
 // URLs adicionales para precache manual
 const urlsToCache = [
