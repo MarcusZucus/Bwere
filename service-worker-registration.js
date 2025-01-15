26051554af671fe3516460bcf6f486f4374d90ed
MarcusZucus <marcozucoia@gmail.com>
2025-01-15 13:38:26 +0100

    Update service-worker-registration.js

--- service-worker-registration.js
+++ service-worker-registration.js
@@ -1,6 +1,7 @@
+// service-worker-registration.js
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/service-worker.js', { type: 'module' })
+        navigator.serviceWorker.register('/service-worker.js')
             .then(function(registration) {
                 console.log('Service Worker registered with scope:', registration.scope);
             })
